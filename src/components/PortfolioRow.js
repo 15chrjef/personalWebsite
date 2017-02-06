a9364c392fe18b3a2a47d9a3f288092243cddbeb
jeff christian <jchri@MacBook-Pro-2.loca>
2017-02-05 20:02:02 -0800

    finish portfolio;

--- src/components/PortfolioRow.js
+++ src/components/PortfolioRow.js
@@ -11,34 +11,36 @@ const PortfolioRow = (props) => {
 		button = 'See on Github'
 	}
 	return (
-		<Row>
-			<Col xs={6}>
-				<Thumbnail src={images[0]} alt="484x400">
-					<h3>{titles[0]}</h3>
-					<p>{descriptions[0]}</p>
-					<p>
-						<a href={links[0]}>
-							<Button bsStyle="primary">
-								{button}
-							</Button>
-						</a>
-					</p>
-				</Thumbnail>
-			</Col>
-			<Col xs={6}>
-				<Thumbnail src={images[1]} alt="484x400">
-					<h3>{titles[1]}</h3>
-					<p>{descriptions[1]}</p>
-					<p>
-						<a href={links[1]}>
-							<Button bsStyle="primary">
-								{button}
-							</Button>
-						</a>
-					</p>
-				</Thumbnail>
-			</Col>
-		</Row>
+		<div style={{marginTop: '30px'}}>
+			<Row>
+				<Col xs={6}>
+					<Thumbnail src={images[0]} alt="484x400">
+						<h3>{titles[0]}</h3>
+						<p>{descriptions[0]}</p>
+						<p>
+							<a href={links[0]}>
+								<Button bsStyle="primary">
+									{button}
+								</Button>
+							</a>
+						</p>
+					</Thumbnail>
+				</Col>
+				<Col xs={6}>
+					<Thumbnail src={images[1]} alt="484x400">
+						<h3>{titles[1]}</h3>
+						<p>{descriptions[1]}</p>
+						<p>
+							<a href={links[1]}>
+								<Button bsStyle="primary">
+									{button}
+								</Button>
+							</a>
+						</p>
+					</Thumbnail>
+				</Col>
+			</Row>
+		</div>
 	)
 }
 
